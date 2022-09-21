fd797f7be433e0de83c020b31122a1fd3839d723
ortizogaima <ortizogaimanol.labase@gmail.com>
2022-09-21 13:21:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div>
+    <img alt="Background" class="background" src="./assets/Background.jpg" />
+    <div class="filter"></div>
+    <div> Soy la slidebar</div>
+    <div class="container"><div class="saludo">Hola, Soy imanol</div></div>
+  </div>
+
+</template>
+
+<style scoped>
+
+.background {
+  display: flex;
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+}
+
+.filter{
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  opacity: 0.3;
+}
+
+.container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: aqua;
+  height: 15rem;
+}
+
+.saludo{
+  display: flex;
+  background-color: blueviolet;
+}
+
+</style>
+
